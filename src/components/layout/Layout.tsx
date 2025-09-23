47525b35c9b7e06100f380715e94a7ab13f37a2e
ahasan0015 <ahasanstu@gmail.com>
2025-09-23 13:12:32 +0600

    react-templeating

---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,49 @@
+import Dashboard from "./Dashboard"
+import Footer from "./Footer"
+import Navbar from "./Navbar"
+import Sidebar from "./Sidebar"
+
+function Layout(){
+    return(
+        <>
+        {/*  Layout wrapper  */}
+      <div className="layout-wrapper layout-content-navbar">
+        <div className="layout-container">
+          {/*  Menu  */}
+
+          <Sidebar/>
+          {/*  / Menu  */}
+
+          {/*  Layout container  */}
+          <div className="layout-page">
+            {/*  Navbar  */}
+
+           <Navbar/>
+
+            {/*  / Navbar  */}
+
+            {/*  Content wrapper  */}
+            <div className="content-wrapper">
+              {/*  Content  */}
+              <Dashboard/>
+              {/*  / Content  */}
+
+              {/*  Footer  */}
+             <Footer/>
+              {/*  / Footer  */}
+
+              <div className="content-backdrop fade"></div>
+            </div>
+            {/*  Content wrapper  */}
+          </div>
+          {/*  / Layout page  */}
+        </div>
+
+        {/*  Overlay  */}
+        <div className="layout-overlay layout-menu-toggle"></div>
+      </div>
+      {/*  / Layout wrapper  */}
+        </>
+    )
+}
+export default Layout
